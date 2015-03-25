8307af89c1f6af93a3193b5843b9156f5a08085f
Evan Dorsky <evan.dorsky@icloud.com>
2015-03-24 23:32:36 -0400

    Canvas fits page

--- ruleof3s/ruleofthrees.js
+++ ruleof3s/ruleofthrees.js
@@ -1,3 +1,27 @@
 (function() {
-	$('body').prepend('<canvas style="margin:0;padding:0;position:absolute;width:100%;height:100%" id="overlay"></canvas>');
+	$('body').prepend('<canvas style="margin:0;padding:0;position:absolute;z-index:99999" id="overlay"></canvas>');
+
+	var canvas = document.getElementById('overlay');
+
+	$(window).on('resize', function() {
+		canvas.height = $(window).height();
+		canvas.width = $(window).width();
+		
+		drawRect(ctx, 20, 20, 200, 200);	
+	});
+
+	var ctx = canvas.getContext('2d');
+
+	drawRect(ctx, 20, 20, 200, 200);
+
+	function drawRect(ctx, x, y, w, h) {
+		ctx.strokeStyle = 'black';
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				ctx.rect(x+i*w/3, y+j*h/3, w/3, h/3);
+			}
+		}
+		ctx.rect(x, y, w, h);
+		ctx.stroke();
+	}
 })()
